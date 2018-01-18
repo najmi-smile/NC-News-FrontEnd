9b1b25cd611ad0a15c58d27b0ca4c6d3152e27e0
najmi-smile <tahiranjam@gmail.com>
2018-01-18 20:58:14 +0000

    added ArticlesPage and TopicsPage

--- src/components/ArticlesPage.js
+++ src/components/ArticlesPage.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import GetArticles from '../httpRequests/GetArticles';
 
-const ArticlesPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+class ArticlesPage extends React.Component {
+  state = {
+    articles: []
+  }
+
+  componentDidMount() {
+    GetArticles()
+    .then (res => {
+      this.setState({
+        articles : res.articles
+      })
+    })
+    .catch(console.log);
+  }
+  render() {
+    const { articles } = this.state;
+    let articleNode;
+    if(articles.length > 0) {
+      articleNode = articles.map(article => {
+        return (
+          <ul key={article._id}>
+            <li><h1>{article.created_by}</h1></li>
+            <li><p>{article.title}</p></li>
+            <li><small>{article.body}</small></li>
+            <li><span>{article.belongs_to}</span></li>
+            <li><span>{article.votes}</span></li>
+            <li><span>{article.comments}</span></li>
+          </ul>
+        )
+      })
+    }
+    return(
+      <div>        
+        <h2>Available Article</h2> 
+        { articles.length > 0 && articleNode }
+          
+      </div>
+    )
+  }
 }
 
 export default ArticlesPage;
--- src/components/TopicsPage.js
+++ src/components/TopicsPage.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const TopicsPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import GetTopics from '../httpRequests/GetTopics.js';
+import Topics from './HomePage-Components/Topics';
+
+class TopicsPage extends React.Component {
+  state = {
+    topics : []
+  }
+
+  componentDidMount(){
+    GetTopics()
+      .then (res => {
+        this.setState({
+        topics: res.topics
+        })
+      })
+      .catch(console.log);   
+  }
+
+  render() {
+    const {topics} = this.state;
+    let TopicNode;
+    if(topics.length > 0) {
+      TopicNode = topics.map(topic => {
+        return (
+          <ul key = {topic._id}>
+            <li>Title : {topic.title}</li>
+            <li>Slug : {topic.slug}</li>
+            <li><small>Click title for related Articles</small></li>
+          </ul>
+        )
+      })
+    }
+    return (
+      <div>
+        <h1>Available Topics</h1>
+        {topics.length > 0 && TopicNode}
+      </div>
+    )
+  }
 }
 
 export default TopicsPage;
