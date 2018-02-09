34730a1cbd73e94b0b03b0857892e425adbb6b11
najmi-smile <tahiranjam@gmail.com>
2018-02-09 12:46:18 +0000

    brought navigation files

---
+++ src/navigator/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NotFound = () => {
+
+  return (
+    <div className="not-found">
+      <div>
+        <h1>404</h1>
+        <p style={{color:'yellow', fontSize:'40px'}}>The Page you are looking for does not exist</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
