8646d4ee3c628f55e0541d554c301446681b8ffb
najmi-smile <tahiranjam@gmail.com>
2018-02-09 12:38:13 +0000

    add http requests

---
+++ src/httpRequests/FetchData.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const FetchData = (url, method) => {
+  method =  method || 'GET'
+  console.log('Before Fetch URL', url);
+  return fetch(`https://quiet-shore-88770.herokuapp.com/api${url}`,{"method":method})
+    .then(buffer => buffer.json())
+    .then(res => {
+      return res;    
+    })
+}
+export const fetchArticles =(url, method) => {
+  method =  method || 'GET'
+  console.log('Before Fetch URL', url);
+  return fetch(`https://quiet-shore-88770.herokuapp.com/api${url}`,{"method":method})
+    .then(buffer => buffer.json())
+    .then(res => {
+      return res;    
+    })
+}
+export const fetchUsers =(url, method) => {
+  method =  method || 'GET'
+  console.log('Before Fetch URL', url);
+  return fetch(`https://quiet-shore-88770.herokuapp.com/api${url}`,{"method":method})
+    .then(buffer => buffer.json())
+    .then(res => {
+      return res;    
+    })
+}
+export const fetchTopics =(url, method) => {
+  method =  method || 'GET'
+  console.log('Before Fetch URL', url);
+  return fetch(`https://quiet-shore-88770.herokuapp.com/api${url}`,{"method":method})
+    .then(buffer => buffer.json())
+    .then(res => {
+      return res;    
+    })
+}
+export const fetchComments =(url, method) => {
+  method =  method || 'GET'
+  console.log('Before Fetch URL', url);
+  return fetch(`https://quiet-shore-88770.herokuapp.com/api${url}`,{"method":method})
+    .then(buffer => buffer.json())
+    .then(res => {
+      return res;    
+    })
+}
+export const fetchWeather = () => {
+  return fetch(
+    "https://fcc-weather-api.glitch.me/api/current?lon=-2.24&lat=53.48"
+  ).then(buffer => buffer.json());
+}
+
+
+
+export default FetchData;
