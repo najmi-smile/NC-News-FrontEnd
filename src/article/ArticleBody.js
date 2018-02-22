329c49b69d57577f6a856ad4beb066b7ca678c27
najmi-smile <tahiranjam@gmail.com>
2018-02-22 08:33:29 +0000

    update article body for unknown users

--- src/article/ArticleBody.js
+++ src/article/ArticleBody.js
@@ -4,24 +4,27 @@ import PT from "prop-types";
 import { Link } from 'react-router-dom';
 import VoteArticle from './voteArticle';
 
-const ArticleBody = ({article,articleUser}) => ( 
-  <div className='column isDark'>
-    <div className="title has-text-white">
-      {article && <Link to={`/users/${articleUser.username}`}>{article.created_by}</Link>}
-      <span className="is-pulled-right subtitle has-text-white">
-        {article && <VoteArticle article={article}/>}
-      </span>
-      <div className="subtitle is-size-6 has-text-danger">
-        {Moment(article && article.created_at).format("dddd Do MMMM YYYY")}
+const ArticleBody = ({article,articleUser}) => {
+  const username =  articleUser.username || 'unknown';
+  return (
+    <div className='column isDark'>
+      <div className="title has-text-white">
+        {article && <Link to={`/users/${username}`}>{article.created_by}</Link>}
+        <span className="is-pulled-right subtitle has-text-white">
+          {article && <VoteArticle article={article}/>}
+        </span>
+        <div className="subtitle is-size-6 has-text-danger">
+          {Moment(article && article.created_at).format("dddd Do MMMM YYYY")}
+        </div>
+      </div>
+
+      <div className="box article-body">
+        <p>{article && article.title}</p>
+        <code className="js customScroll">{article && article.body}</code>
       </div>
     </div>
-
-    <div className="box article-body">
-      <p>{article && article.title}</p>
-      <code className="js customScroll">{article && article.body}</code>
-    </div>
-  </div>
-)
+  )
+}
 
 ArticleBody.propTypes = {
   article: PT.object.isRequired,
